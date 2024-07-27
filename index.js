ff726d58bf2befd457f804011e0419e4aeb498d7
RaviKourav <ravikourav9516@gmail.com>
2024-07-27 17:30:56 +0530

    backend setup completed DB Connected / Express Working

---
+++ index.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import connectDB from './src/db/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
