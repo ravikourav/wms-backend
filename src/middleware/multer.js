f062db6679e3efbacd095bba1b409b05f9e40374
RaviKourav <ravikourav9516@gmail.com>
2024-10-19 00:47:47 +0530

    update code to use vercels tmp folder for temp stroge

--- src/middleware/multer.js
+++ src/middleware/multer.js
@@ -2,10 +2,11 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/temp')
+      cb(null, '/tmp')
     },
     filename: function (req, file, cb) {
-      cb(null, file.originalname)
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+      cb(null, uniqueSuffix + '-' + file.originalname); 
     }
   })
   
